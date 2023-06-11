e5a2c43e73fad1365b29e7ca04f5edd8240df6b9
premiumpeng <pagict@gmail.com>
2023-06-11 10:54:38 +0800

    [confs] script patch_confs.sh

---
+++ patch_confs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PATCH_FILE=incoming.patch
+CURRENT=master
+TARGET=origin/master
+#git fetch
+git diff --no-prefix ${CURRENT} ${TARGET} > ${PATCH_FILE}
+
+ESCAPED_HOME=$(printf '%s\n' "${HOME}" | sed -e 's/[\/&]/\\&/g')
+
+sed -i "s/ confs\/dotprefix\// ${ESCAPED_HOME}\/./g" ${PATCH_FILE}
+
+patch -f -p0 -d / < ${PATCH_FILE}
+git pull --ff-only
+
